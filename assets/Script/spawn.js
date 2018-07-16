1762d121798e89b5aaade0b0e6ee664b9a2fccba
linhdt <linhdt@gemvietnam.com>
2018-07-16 14:45:11 +0700

    add new image

---
+++ assets/Script/spawn.js
@@ -0,0 +1,25 @@
+//  spawnNew: function(){
+//         // var arr = [];
+//         // this.node.addChild(spawn1);
+//         // this.node.addChild(spawn2);  
+//        var x =0;
+//        var y = 0;
+//         for(  i=-7; i< 8 ; i++){
+//             var a = -7;
+//             for(  ; a < 8 ; a++){            
+                                                                           
+//                     var spawn = cc.instantiate(this.PreFaps);                                                        
+//                     this.node.addChild(spawn);                   
+//                      spawn.getComponent('Block').row=a;
+//                      spawn.getComponent('Block').col=i;
+//                      x =a * spawn.width;
+//                      y = i * spawn. height;
+//                     spawn.setPosition(this.getNewStarPosition(x,y));                                                                                               
+//                     this.item.push(spawn);
+//                     spawn.getComponent('Block').Game = this;
+                    
+               
+//             }                      
+//         }    
+   
+//     }
